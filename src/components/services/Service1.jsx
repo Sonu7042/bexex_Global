db598f67e832e254222b59e625aa1aa1c6690a7f
sonu7042 <sonusinghmourya100@gmail.com>
2025-12-13 18:07:52 +0530

    updated

--- src/components/services/Service1.jsx
+++ src/components/services/Service1.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../Css/service1.css";
 import { LearnMoreButton, HeadingComponent } from "../Buttons.jsx";
 import nature from "../../assets/images/nature.jpg";
@@ -43,6 +43,179 @@ const cards = [
 ];
 
 const Service1 = () => {
+  //  first filter
+  const [openMenu, setOpenMenu] = useState(null);
+
+  const [selectedFilter, setSelectedFilter] = useState("Consulting");
+
+  const options = ["Consulting", "Auditing", "Training", "Software Solution"];
+
+  const handleSelect = (opt) => {
+    setSelectedFilter(opt);
+    setOpenMenu(null);
+  };
+
+  // secondary filter
+  const [selectedSecondFilter, setSelectedSecondFilter] = useState(
+    "environment, health & safety solutions"
+  );
+  const secondaryOptions = [
+    "quality & business excellence",
+    "management systems and compliance",
+    "esg and sustainability services",
+    "software & digital solutions",
+    "training & competency development",
+    "environment, health & safety solutions",
+  ];
+
+  const handleSecondSelect = (opt) => {
+    setSelectedSecondFilter(opt);
+    setOpenMenu(null);
+  };
+
+  // filter data according second filter
+  const filterDataforthirdfilter = [
+    // ENVIRONMENT, HEALTH & SAFETY SOLUTIONS
+    {
+      name: "Fire Safety",
+      category: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    },
+    {
+      name: "Electrical Safety",
+      category: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    },
+    {
+      name: "Logistics and Warehouse Safety",
+      category: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    },
+    {
+      name: "Behavior-Based Safety (BBS)",
+      category: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    },
+
+    // QUALITY & BUSINESS EXCELLENCE
+    {
+      name: "Six Sigma",
+      category: "QUALITY & BUSINESS EXCELLENCE",
+    },
+    {
+      name: "5-S Workplace Organization",
+      category: "QUALITY & BUSINESS EXCELLENCE",
+    },
+    {
+      name: "Supplier Quality Management",
+      category: "QUALITY & BUSINESS EXCELLENCE",
+    },
+    {
+      name: "Lean Management",
+      category: "QUALITY & BUSINESS EXCELLENCE",
+    },
+    {
+      name: "Total Productive Maintenance (TPM)",
+      category: "QUALITY & BUSINESS EXCELLENCE",
+    },
+
+    // MANAGEMENT SYSTEMS AND COMPLIANCE
+    {
+      name: "ISO System Consulting",
+      category: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    },
+    {
+      name: "Automotive & Aerospace Standards",
+      category: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    },
+    {
+      name: "Miscellaneous Services",
+      category: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    },
+    {
+      name: "Legal Compliance Management",
+      category: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    },
+
+    // ESG AND SUSTAINABILITY SERVICES
+    {
+      name: "ESG Strategy & Reporting",
+      category: "ESG AND SUSTAINABILITY SERVICES",
+    },
+    {
+      name: "Energy Management",
+      category: "ESG AND SUSTAINABILITY SERVICES",
+    },
+    {
+      name: "Environmental Compliance",
+      category: "ESG AND SUSTAINABILITY SERVICES",
+    },
+    {
+      name: "Water & Waste Management",
+      category: "ESG AND SUSTAINABILITY SERVICES",
+    },
+    {
+      name: "Social & Welfare",
+      category: "ESG AND SUSTAINABILITY SERVICES",
+    },
+
+    // SOFTWARE & DIGITAL SOLUTIONS
+    {
+      name: "Custom EHS Software",
+      category: "SOFTWARE & DIGITAL SOLUTIONS",
+    },
+    {
+      name: "Digital LMS Platforms",
+      category: "SOFTWARE & DIGITAL SOLUTIONS",
+    },
+    {
+      name: "Performance Dashboards",
+      category: "SOFTWARE & DIGITAL SOLUTIONS",
+    },
+    {
+      name: "Digital Resources & Toolkits",
+      category: "SOFTWARE & DIGITAL SOLUTIONS",
+    },
+
+    // TRAINING & COMPETENCY DEVELOPMENT
+    {
+      name: "Management Systems Training",
+      category: "TRAINING & COMPETENCY DEVELOPMENT",
+    },
+    {
+      name: "Leadership & Soft Skills",
+      category: "TRAINING & COMPETENCY DEVELOPMENT",
+    },
+    {
+      name: "Technical Training",
+      category: "TRAINING & COMPETENCY DEVELOPMENT",
+    },
+    {
+      name: "Quality Tools Training",
+      category: "TRAINING & COMPETENCY DEVELOPMENT",
+    },
+    {
+      name: "Competency Management",
+      category: "TRAINING & COMPETENCY DEVELOPMENT",
+    },
+  ];
+
+  const safetyServices = filterDataforthirdfilter.filter(
+    (item) => item.category.toLowerCase() === selectedSecondFilter.toLowerCase()
+  );
+
+  console.log(safetyServices, "safetyServices");
+
+  // third filter
+  const [selectedthirdFilter, setselectedthirdFilter] = useState("");
+
+  useEffect(() => {
+    if (safetyServices.length > 0) {
+      setselectedthirdFilter(safetyServices[0].name);
+    }
+  }, [selectedSecondFilter]);
+
+  const handlethirdSelect = (opt) => {
+    setselectedthirdFilter(opt);
+    setOpenMenu(null);
+  };
+
   return (
     <>
       {/* service card rendering  */}
@@ -59,17 +232,89 @@ const Service1 = () => {
             <div className="sfBarFiltersRow">
               {/* <span className="sfBarFilterLabel">Filter</span> */}
 
-              <button className="sfBarPill sfBarPill--active">
-                <span className="sfBarPillIcon">✔</span>
-                <span className="sfBarPillText">Consulting</span>
-              </button>
+              {/* pill with dropdown */}
+              <div className="sfBarPillDropdown">
+                <button
+                  type="button"
+                  className="sfBarPill sfBarPill--active"
+                  onClick={() =>
+                    setOpenMenu(openMenu === "first" ? null : "first")
+                  }
+                >
+                  <span className="sfBarPillIcon">✔</span>
+                  <span className="sfBarPillText">{selectedFilter}</span>
+                </button>
 
-              <button className="sfBarPill">
-                <span className="sfBarPillIcon">✔</span>
-                <span className="sfBarPillText">
-                  Environment, health &amp; safety (ehs) solution
-                </span>
-              </button>
+                {openMenu === "first" && (
+                  <div className="sfBarPillMenu">
+                    {options.map((opt) => (
+                      <button
+                        key={opt}
+                        className="sfBarPillMenuItem"
+                        onClick={() => handleSelect(opt)}
+                      >
+                        {opt}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+
+              {/*  second filter pill with dropdown */}
+              <div className="sfBarPillDropdown">
+                <button
+                  type="button"
+                  className="sfBarPill sfBarPill--active"
+                  onClick={() =>
+                    setOpenMenu(openMenu === "second" ? null : "second")
+                  }
+                >
+                  <span className="sfBarPillIcon">✔</span>
+                  <span className="sfBarPillText">{selectedSecondFilter}</span>
+                </button>
+
+                {openMenu === "second" && (
+                  <div className="sfBarPillMenu">
+                    {secondaryOptions.map((opt) => (
+                      <button
+                        key={opt}
+                        className="sfBarPillMenuItem"
+                        onClick={() => handleSecondSelect(opt)}
+                      >
+                        {opt}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+
+              {/* third filter pill with dropdown */}
+              <div className="sfBarPillDropdown">
+                <button
+                  type="button"
+                  className="sfBarPill sfBarPill--active"
+                  onClick={() =>
+                    setOpenMenu(openMenu === "third" ? null : "third")
+                  }
+                >
+                  <span className="sfBarPillIcon">✔</span>
+                  <span className="sfBarPillText">{selectedthirdFilter}</span>
+                </button>
+
+                {openMenu === "third" && (
+                  <div className="sfBarPillMenu">
+                    {safetyServices.map((opt) => (
+                      <button
+                        key={opt.name} // ✅ unique string key
+                        className="sfBarPillMenuItem"
+                        onClick={() => handlethirdSelect(opt.name)} // ✅ pass name only
+                      >
+                        {opt.name} {/* ✅ render string */}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
 
--- src/dataStore/serviceData.js
+++ src/dataStore/serviceData.js
@@ -1,53 +1,620 @@
- 
- 
- import img1 from '../assets/images/nature.jpg'
- 
- export const data = [
+import img1 from "../assets/images/nature.jpg";
+
+export const data = [
   {
     id: 1,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 2,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 3,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 4,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 5,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 6,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 7,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 8,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
   {
     id: 9,
-    img:img1,
-    des:"About the Serivce"
+    img: img1,
+    des: "About the Serivce",
   },
- 
+];
+
+const servicesData = [
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Six Sigma",
+    value: "DMAIC Project Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Six Sigma",
+    value: "DMADV Project Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Six Sigma",
+    value: "Green Belt Project Mentoring",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Six Sigma",
+    value: "Black Belt Project Mentoring",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Six Sigma",
+    value: "Statistical Process Control (SPC)",
+  },
+
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "5-S Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "5-S Trainings",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "5-S Workshops",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "5-S Audits",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "Visual Management System Design",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "Visual Management System Development",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "Standard Operating Procedures Creation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "5-S Workplace Organization",
+    value: "Work Instruction Creation",
+  },
+
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Supplier Quality Management",
+    value: "Supplier Assessment",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Supplier Quality Management",
+    value: "Supplier Development Programs",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Supplier Quality Management",
+    value: "Supplier Audits",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Supplier Quality Management",
+    value: "Supplier Performance Monitoring",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Supplier Quality Management",
+    value: "Collaborative Quality Improvement Initiatives",
+  },
+
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Kaizen Event Facilitation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Waste Reduction – Muda",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Waste Reduction – Muri",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Waste Reduction – Mura",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Just-In-Time (JIT) Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Kanban System Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Lean Management",
+    value: "Value Stream Mapping (VSM)",
+  },
+
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "Overall Equipment Effectiveness (OEE) Analysis",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "Overall Equipment Effectiveness (OEE) Improvement",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "TPM Pillar Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "TPM Training",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "Autonomous Maintenance Program Implementation",
+  },
+  {
+    title: "QUALITY & BUSINESS EXCELLENCE",
+    category: "Total Productive Maintenance (TPM)",
+    value: "Planned Maintenance Program Implementation",
+  },
+
+  // FIRE SAFETY
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Fire Risk Assessments as per IS Standards",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Fire Risk Assessments as per NBC",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Design Audits of Fire Detection Systems",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Adequacy Audits of Fire Protection Systems",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Emergency Response Planning",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Evacuation Planning (ERDMP)",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Supply of Fire Systems",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Installation of Fire Systems",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Fire Detection Mapping",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Fire Safety",
+    value: "Fire Gas Detection Mapping",
+  },
+
+  // ELECTRICAL SAFETY
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Arc Flash Risk Assessments",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Hazard Analysis",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Electrical Safety Audits",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Infrared Thermography Studies",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Short Circuit Studies",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Relay Coordination Studies",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Harmonics Studies",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Lockout Tagout (LOTO) Procedure Development",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "Lockout Tagout (LOTO) Mapping",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Electrical Safety",
+    value: "LPS Adequacy Audits",
+  },
+
+  // CONSTRUCTION SAFETY
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Comprehensive Construction Safety Management at Site",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Contractor Safety Management",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Contractor Safety Evaluation",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Ladder Inspection",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Scaffolding Inspection",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Scaffolding Certification",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Work at Height Safety Programs",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Construction Safety",
+    value: "Lifting Safety Programs",
+  },
+
+  // LOGISTICS & WAREHOUSE SAFETY
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Logistics and Warehouse Safety",
+    value: "Warehouse Risk Assessment",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Logistics and Warehouse Safety",
+    value: "Warehouse Layout Design",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Logistics and Warehouse Safety",
+    value: "Transportation Safety Management",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Logistics and Warehouse Safety",
+    value: "Fleet Audits",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Logistics and Warehouse Safety",
+    value: "Journey Risk Management",
+  },
+
+  // BEHAVIOR-BASED SAFETY
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "Safety Culture Assessment",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "Safety Culture Transformation",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "BBS Program Design",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "BBS Program Implementation",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "Safety Observation Training",
+  },
+  {
+    title: "ENVIRONMENT, HEALTH & SAFETY SOLUTIONS",
+    category: "Behavior-Based Safety (BBS)",
+    value: "Safety Feedback Training",
+  },
+
+  // ISO SYSTEM CONSULTING
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 9001 QMS"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14001 EMS"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14046 Water Footprint"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14064 Part 1"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14064 Part 2"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14064 Part 3"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 14090 Climate Change"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 50001 EnMS"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 22000 Food Safety"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 39001 Road Safety"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 45001 OHSMS"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 22301 Business Continuity"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 28001 Security Management"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 31001 Business Risk"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 55001 Asset Management"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 17025 Laboratories"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 20000 Information Technology"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 27001 Information Security"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "ISO System Consulting",
+    value: "ISO 21001 Educational Organizations Management"
+  },
+
+  // AUTOMOTIVE & AEROSPACE STANDARDS
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Automotive & Aerospace Standards",
+    value: "AS 9100 QMS for Aerospace"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Automotive & Aerospace Standards",
+    value: "IATF 16949 QMS for Automotive"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Automotive & Aerospace Standards",
+    value: "Core QC Tools Implementation"
+  },
+
+  // LEGAL COMPLIANCE MANAGEMENT
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Legal Compliance Management",
+    value: "EHSEN Compliances Audits"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Legal Compliance Management",
+    value: "Legal Register Preparation"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Legal Compliance Management",
+    value: "Legislation Trainings"
+  },
+
+  // MISCELLANEOUS SERVICES
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Improvement (CI) Initiatives"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Efficiency Optimization Toolkit"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Integration of ISO Systems"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "ISO Internal (Shadow) Audit"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Manual Preparation"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "SOP Preparation"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Work Instruction Preparation"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Excellence Module by eGrowth"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Energy Audit"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Energy Review and Performance"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Environmental Review (EAIA)"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Hazard Identification (HIRA)"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Metro SHE Audits"
+  },
+  {
+    title: "MANAGEMENT SYSTEMS AND COMPLIANCE",
+    category: "Miscellaneous Services",
+    value: "Metro Quality Audit"
+  }
 ];
 
